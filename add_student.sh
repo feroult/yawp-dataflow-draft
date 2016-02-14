1c861900751ca12fb3f0d26ebd65787ae711cc6a
feroult <feroult@gmail.com>
2016-02-14 10:30:38 -0200

    pipes

--- add_student.sh
+++ add_student.sh
@@ -3,4 +3,4 @@
 STUDENT_ID=$1
 GRADE_ID=$2
 
-curl -H "Content-type: application/json" -X POST -d "{ id: '/students/$STUDENT_ID', gradeId: '/grades/$GRADE_ID' }" http://localhost:8080/api/students/put; echo
+curl -H "Content-type: application/json" -X POST -d "{ id: '/students/$STUDENT_ID', gradeId: '/grades/$GRADE_ID' }" http://localhost:8080/api/students; echo
